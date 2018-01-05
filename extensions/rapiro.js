d422b80f2d6ab9b9b0278ec22e40146dfbe4107f
P306 <jyang@stust.edu.tw>
2018-01-05 13:22:43 +0800

    d

--- extensions/rapiro.js
+++ extensions/rapiro.js
@@ -56,7 +56,7 @@ Send Raipro Commands via wf8266r
         var uri = 'http://'+ip+'/serial/write?text='+cmd;
         $.ajax({
             url: uri,
-            type: 'POST',
+            type: 'GET',
             success: function (data) {
                 restRet = data;
             },
@@ -70,7 +70,7 @@ Send Raipro Commands via wf8266r
         var uri = 'http://127.0.0.1:5000'+cmd;
         $.ajax({
             url: uri, 
-            type: 'POST',
+            type: 'GET',
             success: function (data) {
                 restRet = data;
             },
